8fc6f8b532b7768391200a9080613aaad67d16b4
volbok <rodrigocarvalholessa@gmail.com>
2023-11-06 17:31:17 -0300

    atualizando api

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 var cors = require("cors");
 const app = express();
-const PORT = process.env.PORT || 3333;
+const port = process.env.PORT || 3333;
 
 app.use(cors());
 app.use(function (req, res, next) {
